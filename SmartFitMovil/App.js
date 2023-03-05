92406edbf04e32514da54184dbb90b855b4accbd
CaroJimenez <105475884+CaroJimenez@users.noreply.github.com>
2023-03-05 11:34:25 -0600

    Add files via upload

---
+++ SmartFitMovil/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import 'react-native-gesture-handler';
+import AppNavigation from './src/navigation/AppNavigation';
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+             <AppNavigation/>
+        </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ SmartFitMovil/src/screens/PasswordScreen.js
@@ -0,0 +1,18 @@
+import {TextInput} from 'react-native';
+import React from "react";
+import { StyleSheet } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, SafeAreaView, Image } from 'react-native';
+import colors from "../utils/colors";
+
+export default function PasswordScreen() {
+    return (
+        <>
+       <Text>Recupera tu contraseña</Text>
+       
+        </>
+    );
+    }
+
+    const styles = StyleSheet.create({
+        
+    });
