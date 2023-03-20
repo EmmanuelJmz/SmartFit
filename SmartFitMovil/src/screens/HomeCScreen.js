58fc78d0fd4d2097ac10407b08146ab582d81c92
CaroJimenez <105475884+CaroJimenez@users.noreply.github.com>
2023-03-20 09:24:17 -0600

    Primer commit
    Contiene un login, un registro y un formulario para recuperar contraseña

---
+++ SmartFitMovil/src/screens/HomeCScreen.js
@@ -0,0 +1,17 @@
+import {TextInput} from 'react-native';
+import React from "react";
+import { StyleSheet } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, SafeAreaView, Image } from 'react-native';
+import colors from "../utils/colors";
+
+export default function HomeCScreen() {
+    return (
+        <>
+            <Text>HomeCScreen</Text>
+        </>
+    );
+    }
+
+    const styles = StyleSheet.create({
+        
+    });
